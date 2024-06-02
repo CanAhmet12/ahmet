1c62f408d700c30829602bfa57a4ef9af998f473
CanAhmet12 <can65385@gmail.com>
2024-06-02 21:07:52 +0300

    ahmet

---
+++ KütapaneÖDEVPROJE/Class1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Windows.Forms;
+
+namespace KütapaneÖDEVPROJE
+{
+    internal class Kütüphane
+    {
+        public static int KitapID = 0;
+        public static int UserID = 0;
+
+        public static bool serach = false;
+        public static string constr = "Data Source=.\\SQLEXPRESS;Initial Catalog=BookSave;Integrated Security=True;";
+        public static string constr2 = "Data Source=.\\SQLEXPRESS;Initial Catalog=UserSave;Integrated Security=True;";
+        public static void BookSave(int ID, string KitapAdı, string BarkodNo, string Yazar, int SayfaNo, string Tür, string Dil, string YayınEvi, string Yıl, string KayıtTarihi)
+        {
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand com = new SqlCommand(" if not exists  (select * from Book where ID = @ID) insert into Book (BarkodNo , KitapAdı , Yazar , SayfaNo , Tür , Dil , YayınEvi , Yıl , KayıtTarihi) values (@BarkodNo , @KitapAdı , @Yazar , @SayfaNo , @Tür , @Dil , @YayınEvi , @Yıl , @KayıtTarihi) else update Book set   BarkodNo= @BarkodNo , KitapAdı=@KitapAdı , Yazar=@Yazar , SayfaNo=@SayfaNo , Tür=@Tür , Dil=@Dil , YayınEvi=@YayınEvi , Yıl=@Yıl , KayıtTarihi= @KayıtTarihi where ID = @ID", con);
+            com.Parameters.AddWithValue("@ID", KitapID);
+            com.Parameters.AddWithValue("@KitapAdı", KitapAdı);
+            com.Parameters.AddWithValue("@BarkodNo", BarkodNo);
+            com.Parameters.AddWithValue("@Yazar", Yazar);
+            com.Parameters.AddWithValue("@SayfaNo", SayfaNo);
+            com.Parameters.AddWithValue("Tür", Tür);
+            com.Parameters.AddWithValue("@Dil", Dil);
+            com.Parameters.AddWithValue("@YayınEvi", YayınEvi);
+            com.Parameters.AddWithValue("@Yıl", Yıl);
+            com.Parameters.AddWithValue("@KayıtTarihi", DateTime.Now.ToString("dd - MM - yyyy"));
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+
+
+        }
+        public static void UserSave(int ID, String Adı, string Soyadı, string Meslek, string Şehir, string DoğumTarihi, string KullanıcıAdı, string Şifre, string şifreTekrar)
+        {
+            SqlConnection con = new SqlConnection(constr2);
+            SqlCommand com = new SqlCommand("if not exists (select * from User1 where ID = @ID) insert into User1 (Adı , Soyadı , Meslek , Şehir , DoğumTarihi , KullanıcıAdı , Şifre , ŞifreTekrar) values ( @Adı ,@Soyadı ,@Meslek ,@Şehir , @DoğumTarihi , @KullanıcıAdı  , @Şifre , @ŞifreTekrar) else update User1 set   Adı=@Adı , Soyadı=@Soyadı , Meslek=@Meslek , Şehir=@Şehir , DoğumTarihi=@DoğumTarihi , KullanıcıAdı=@KullanıcıAdı  , Şifre=@Şifre , ŞifreTekrar=@ŞifreTekrar   where @ID =@ID", con);
+            com.Parameters.AddWithValue("@ID", UserID);
+            com.Parameters.AddWithValue("@Adı", Adı);
+            com.Parameters.AddWithValue("@Soyadı", Soyadı);
+            com.Parameters.AddWithValue("@Meslek", Meslek);
+            com.Parameters.AddWithValue("@Şehir", Şehir);
+            com.Parameters.AddWithValue("@DoğumTarihi", DoğumTarihi);
+            com.Parameters.AddWithValue("@KullanıcıAdı", KullanıcıAdı);
+            com.Parameters.AddWithValue("@Şifre", Şifre);
+            com.Parameters.AddWithValue("@ŞifreTekrar", şifreTekrar);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+
+
+        }
+        public static DataSet GetBooks()
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection(constr);
+            SqlDataAdapter da = new SqlDataAdapter("select ID as [ID] , KitapAdı as [Kitap Adı] , BarkodNo as [Barkod Numarası] , Yazar as [Yazar] , SayfaNo as [Sayfa Numarası] , Tür as [Tür] , Dil as [Dil] , YayınEvi  as [Yayın Evi] , Yıl as [Yıl] , KayıtTarihi  as [Arşive Kayıt Tarihi] from Book  ", con);
+            da.Fill(ds);
+            return ds;
+
+        }
+        public static DataSet GetUser()
+        {
+            DataSet ds = new DataSet();
+            SqlConnection con = new SqlConnection(constr2);
+            SqlDataAdapter da = new SqlDataAdapter("select ID as [ID] , Adı [Adı] , Soyadı as [Soyadı] , Meslek as [Meslek] , Şehir as [Şehir] , DoğumTarihi as [Doğum Tarihi] , KullanıcıAdı as [Kullanıcı Adı] , Şifre as [Şifre] from User1   ", con);
+            da.Fill(ds);
+            return ds;
+
+
+        }
+        public static void DeleteBook()
+        {
+
+            SqlConnection con = new SqlConnection(constr);
+            SqlCommand com = new SqlCommand("Delete from Book where ID=@ID", con);
+            com.Parameters.AddWithValue("ID", KitapID);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+
+        }
+        public static void DeleteUser()
+        {
+
+            SqlConnection con = new SqlConnection(constr2);
+            SqlCommand com = new SqlCommand("Delete from User1 where ID=@ID", con);
+            com.Parameters.AddWithValue("ID", UserID);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+        public static DataSet BookSource(string text, string radiobutton)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "";
+                SqlCommand com = new SqlCommand();
+
+                switch (radiobutton)
+                {
+                    case "BarkodNo":
+                        query = "SELECT ID, KitapAdı, BarkodNo, Yazar, SayfaNo, Tür, Dil, YayınEvi, Yıl, KayıtTarihi FROM Book WHERE BarkodNo LIKE @text";
+                        break;
+                    case "KitapAdı":
+                        query = "SELECT ID, KitapAdı, BarkodNo, Yazar, SayfaNo, Tür, Dil, YayınEvi, Yıl, KayıtTarihi FROM Book WHERE KitapAdı LIKE @text";
+                        break;
+                    case "Yazar":
+                        query = "SELECT ID, KitapAdı, BarkodNo, Yazar, SayfaNo, Tür, Dil, YayınEvi, Yıl, KayıtTarihi FROM Book WHERE Yazar LIKE @text";
+                        break;
+                }
+
+                com = new SqlCommand(query, con);
+                com.Parameters.AddWithValue("@text", "%" + text + "%");
+
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+            }
+            return ds;
+        }
+        public static void UserUpdate(int ID, string adı, string soyadı, string Meslek, string Şehir, string DoğumTarihi, string KullanıcıAdı, string Şifre, string ŞifreTekrar)
+        {
+            using (SqlConnection con = new SqlConnection(constr2))
+            {
+                string query = @"
+        IF EXISTS (SELECT * FROM User1 WHERE ID = @ID)
+        BEGIN
+            UPDATE User1 
+            SET Adı=@Adı, Soyadı=@Soyadı, Meslek=@Meslek, Şehir=@Şehir, DoğumTarihi=@DoğumTarihi, KullanıcıAdı=@KullanıcıAdı, Şifre=@Şifre, ŞifreTekrar=@ŞifreTekrar 
+            WHERE ID=@ID
+        END
+        ELSE
+        BEGIN
+            PRINT 'User not found';
+        END";
+
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    // Parametreler ekleniyor
+                    command.Parameters.AddWithValue("@ID", UserID);
+                    command.Parameters.AddWithValue("@Adı", adı);
+                    command.Parameters.AddWithValue("@Soyadı", soyadı);
+                    command.Parameters.AddWithValue("@Meslek", Meslek);
+                    command.Parameters.AddWithValue("@Şehir", Şehir);
+                    command.Parameters.AddWithValue("@DoğumTarihi", DoğumTarihi);
+                    command.Parameters.AddWithValue("@KullanıcıAdı", KullanıcıAdı);
+                    command.Parameters.AddWithValue("@Şifre", Şifre);
+                    command.Parameters.AddWithValue("@ŞifreTekrar", ŞifreTekrar);
+
+                    // Bağlantı açılıyor ve komut çalıştırılıyor
+                    con.Open();
+                    command.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+
+
+        }
+
+    }
+}
+    
+
+
+
+
+
+
+
---
+++ KütapaneÖDEVPROJE/Form9.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace KütapaneÖDEVPROJE
+{
+    partial class Form9
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(46, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rezervasyon Başlangıcı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(154, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "İlçe :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(126, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kullanıcı Adı :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(327, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 30);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(327, 130);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(236, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ONAYLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(327, 177);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(82, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Rezervasyon Bitişi :";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(327, 226);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // Form9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(687, 383);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form9";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
